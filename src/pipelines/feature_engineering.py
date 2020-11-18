51e53bd974943db6dbf1d4e3c93cb984ca224f5f
CaroAcostaTovany <carolina.acostatovany@gmail.com>
2020-11-18 17:39:51 -0600

    Agregar facet grid por años en incidentes y clasificación, más estructura de feature_engineering

---
+++ src/pipelines/feature_engineering.py
@@ -0,0 +1,39 @@
+"""
+Módulo feature engineering.
+Nuestro target es...
+Tenemos un problema priorización de recursos? Creo que prevención.
+"""
+
+def load_transformation(path):
+    """
+    Cargar pickle que se generó durante la transformación
+    :param path: Path donde se encuentra el pickle
+    :return:
+    """
+
+
+def feature_generation(df):
+    """
+    Crear nuevos features útiles.
+
+    :param df: Dataframe del cual se generarán nuevas variables
+    :return:
+    """
+
+    return df
+
+
+def feature_selection(df):
+    """
+    Seleccionaremos las variables importantes
+    :param df: Dataframe del que se seleccionarán variables.
+    :return:
+    """
+
+def save_fe(df, path):
+    """
+    Guardar el dataframe en un pickle
+    :param df: Dataframe que ya tiene los features que se ocuparán.
+    :param path:
+    :return:
+    """
