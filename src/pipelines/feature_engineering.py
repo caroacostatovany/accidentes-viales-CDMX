168411b4241ca45963feb1e1aa0d47fda2ab6899
CaroAcostaTovany <carolina.acostatovany@gmail.com>
2020-11-25 18:27:57 -0600

    Agregar TimeSeriesSplit

--- src/pipelines/feature_engineering.py
+++ src/pipelines/feature_engineering.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, TimeSeriesSplit
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import VarianceThreshold
